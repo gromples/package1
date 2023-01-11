1f47e16975db8c5625d45f02047f175f88da5c7a
Graham Botha <graham.botha@bcx.co.za>
2023-01-11 09:22:55 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gromples/package1
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"log"
+
+	luno "github.com/luno/luno-go"
+)
+
+var (
+	apiKeyID     = flag.String("api_key_id", "j42v5mwx72evv", "Luno API key ID")
+	apiKeySecret = flag.String("api_key_secret", "E1LlaWNjWco8HaEexBkHuTbtRwjuc6v0EQSFf4U-7lU", "Luno API key secret")
+	debug        = flag.Bool("debug", false, "Enable debug mode")
+)
+
+func GetEthID() (string,error) {
+	flag.Parse()
+
+	cl := luno.NewClient()
+	cl.SetDebug(*debug)
+	cl.SetAuth(*apiKeyID, *apiKeySecret)
+
+	ctx := context.Background()
+	req := luno.GetBalancesRequest{}
+	res, err := cl.GetBalances(ctx, &req)
+	if err != nil {
+		log.Println(err)
+		return "",err
+	}
+	log.Printf("\nMy Balance:%+v", res.Balance[1])
+	if res != nil && len(res.Balance) > 0 {
+		return res.Balance[1].AccountId,nil
+	}
+	return "",errors.New("No account ID available")
+
+}
